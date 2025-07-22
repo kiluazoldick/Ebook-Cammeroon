e208f0de87e0f264157669fac98deed8b362d412
Kilua Zoldick <nangadoumer@gmail.com>
2025-07-22 15:34:01 +0100

    Ajouts du chatbot a lappli

--- src/app/dashboard/layout.tsx
+++ src/app/dashboard/layout.tsx
@@ -1,4 +1,5 @@
 import Sidebar from "@/components/Sidebar/Sidebar";
+import Script from "next/script";
 
 export default function DashboardLayout({
   children,
@@ -9,6 +10,16 @@ export default function DashboardLayout({
     <div className="min-h-screen bg-white flex">
       <Sidebar />
       <main className="flex-1 ml-0 lg:ml-64 transition-all">{children}</main>
+
+      {/* Scripts pour le chatbot Botpress */}
+      <Script
+        src="https://cdn.botpress.cloud/webchat/v3.2/inject.js"
+        strategy="afterInteractive"
+      />
+      <Script
+        src="https://files.bpcontent.cloud/2025/07/22/14/20250722141018-76CPQJT5.js"
+        strategy="afterInteractive"
+      />
     </div>
   );
 }
