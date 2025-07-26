cc78bf3a60eadf0c0eea7de657f78f787ebc1f02
Kilua Zoldick <nangadoumer@gmail.com>
2025-07-26 15:01:38 +0100

    Ajouts de l'authentification google

--- src/app/auth/inscription/page.tsx
+++ src/app/auth/inscription/page.tsx
@@ -40,7 +40,7 @@ export default function RegisterPage() {
         emailRedirectTo:
           process.env.NODE_ENV === "development"
             ? "http://localhost:3000/dashboard"
-            : "https://ebookcameroun.com/dashboard",
+            : "https://ebookcameroon.vercel.app/dashboard",
         data: {
           full_name: fullName,
         },
@@ -73,7 +73,7 @@ export default function RegisterPage() {
         redirectTo:
           process.env.NODE_ENV === "development"
             ? "http://localhost:3000/dashboard"
-            : "https://ebookcameroun.com/dashboard",
+            : "https://ebookcameroon.vercel.app/dashboard",
         queryParams: {
           access_type: "offline",
           prompt: "consent",
