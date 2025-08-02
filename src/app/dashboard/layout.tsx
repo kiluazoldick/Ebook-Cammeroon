62966ba637e10c0997ff15ef62807cebe9b60528
Kilua Zoldick <nangadoumer@gmail.com>
2025-08-02 11:19:26 +0100

    Remise du chatbot

--- src/app/dashboard/layout.tsx
+++ src/app/dashboard/layout.tsx
@@ -12,14 +12,14 @@ export default function DashboardLayout({
       <main className="flex-1 ml-0 lg:ml-64 transition-all">{children}</main>
 
       {/* Scripts pour le chatbot Botpress */}
-      {/* <Script
+      <Script
         src="https://cdn.botpress.cloud/webchat/v3.2/inject.js"
         strategy="afterInteractive"
       />
       <Script
         src="https://files.bpcontent.cloud/2025/07/22/14/20250722141018-76CPQJT5.js"
         strategy="afterInteractive"
-      /> */}
+      /> 
     </div>
   );
 }
